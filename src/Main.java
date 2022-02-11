5a6d45d1c4a068e47c2a13b36d0606650f5b5f42
Rodion Kotov <rodion.kotov@internet.ru>
2022-02-11 21:58:59 +0700

    V1.1

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,13 @@
+import java.io.File;
 import java.util.Set;
 
 public class Main {
-
     public static void main(String[] args) {
-        String file_name = "C:\\Users\\rodion\\Desktop\\JAVA\\untitled\\src\\input.txt";
-
-        ParserFile parserFile = new ParserFile(file_name);
-        parserFile.parserFile();
-        parserFile.statisticsInCsvFile("C:\\Users\\rodion\\Desktop\\JAVA\\untitled\\src\\out.txt");
-
+       if (args.length==1){
+           String file_name = args[0];
+           ParserFile parserFile = new ParserFile(file_name);
+           parserFile.parserFile();
+           parserFile.statisticsInCsvFile("statistic.csv");
+       }
     }
 }
